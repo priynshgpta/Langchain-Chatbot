db7ff51125a99c110de89fa416759e0b9da1f74f
Priyansh Gupta <71815344+priynshgpta@users.noreply.github.com>
2025-02-15 18:08:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-HUGGINGFACEHUB_API_TOKEN = "your-huggingface-api-token-here" #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
+HUGGINGFACEHUB_API_TOKEN = "your-huggingface-api-token-here"
 BASE_URL = "your_base_url" 
 VECTOR_STORE_PATH = "data/vector_store"
